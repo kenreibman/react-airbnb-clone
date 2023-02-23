592e4364e755c584bfec5a317b901a5e326a8f9c
kenreibman <reibmanken@gmail.com>
2023-02-22 22:08:28 -0500

    update file path

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,21 +1,24 @@
-import React from "react";
-import HeroImage from "../images/group.png";
+import React from 'react';
+import HeroImage from '../images/group.png';
 
 export default function Hero() {
-    return (
-        <section className="s-hero" aria-labelledby="h2_hero">
-            <h2 className="hide" id="h2_hero">Hero</h2>
-            <div className="s-hero__inner row">
-                <div className="s-hero__container flex">
-                    <img src={HeroImage} alt="Group Collage" />
-                </div>
-                <div className="s-hero__details flex">
-                    <div className="s-hero__title">Online Experiences</div>
-                    <div className="s-hero__subtitle">
-                        Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section className="s-hero" aria-labelledby="h2_hero">
+      <h2 className="hide" id="h2_hero">
+        Hero
+      </h2>
+      <div className="s-hero__inner row">
+        <div className="s-hero__container flex">
+          <img src={HeroImage} alt="Group Collage" />
+        </div>
+        <div className="s-hero__details flex">
+          <div className="s-hero__title">Online Experiences</div>
+          <div className="s-hero__subtitle">
+            Join unique interactive activities led by one-of-a-kind hosts—all
+            without leaving home.
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
